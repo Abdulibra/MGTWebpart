38a1f7ed7f961e0c2a367d6744e6d0d26a6e417f
Abdullahi Ibrahim Ahmed <abdullahi.ibrahim.ahmed@inmeta.no>
2023-06-17 18:04:29 +0200

    removed unused components

--- src/webparts/mgtDemo/components/MgtDemo.tsx
+++ src/webparts/mgtDemo/components/MgtDemo.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styles from './MgtDemo.module.scss';
 import { IMgtDemoProps } from './IMgtDemoProps';
-import { Login, PeoplePicker, Person } from '@microsoft/mgt-react/dist/es6/spfx';
+import { PeoplePicker, Person } from '@microsoft/mgt-react/dist/es6/spfx';
 import { ViewType } from '@microsoft/mgt-spfx';
 
 const MgtDemo: React.FC<IMgtDemoProps> = (props) => {
@@ -11,6 +11,7 @@ const MgtDemo: React.FC<IMgtDemoProps> = (props) => {
     environmentMessage,
     hasTeamsContext,
   } = props;
+  //startPeople contains two users object ids, you can get from users object id in your azure portal.
   const startPeople: string[] = ["33e38fbb-2645-461d-a430-97ab224bb768", "ebe35fd9-72c2-4bd6-b941-ca98c248a76a"]
   return (
     <section className={`${styles.mgtDemo} ${hasTeamsContext ? styles.teams : ''}`}>
@@ -20,11 +21,9 @@ const MgtDemo: React.FC<IMgtDemoProps> = (props) => {
         <div>{environmentMessage}</div>
       </div>
       <div>
-        <Login />
         <Person personQuery='me' view={ViewType.threelines} />
-        <PeoplePicker placeholder='Find your colleague' selectionMode='single' userIds={startPeople} />
         <PeoplePicker
-          placeholder={'Up to 50 people'}
+          placeholder={'Find your colleague'}
           selectionMode={'multiple'}
           id="AdditionalRecipients"
           defaultSelectedUserIds={startPeople}
